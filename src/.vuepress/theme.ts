46de4af498a899430f9d634934fe27b7cacb8366
Leandro Souza <leandro.costa@ifba.edu.br>
2026-03-30 14:42:01 -0300

    hostname

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -4,7 +4,7 @@ import navbar from "./navbar.js";
 import sidebar from "./sidebar.js";
 
 export default hopeTheme({
-  //hostname: "https://mister-hope.github.io",
+  hostname: "https://2026-ifba-saj-inf-poo.github.io/blog",
 
   author: {
     name: "Leandro Souza",
